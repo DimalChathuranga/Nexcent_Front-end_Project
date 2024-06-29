bbea5bea857abd2505f74c59a098d25ecc8d75ab
DimalChathuranga <145420784+DimalChathuranga@users.noreply.github.com>
2024-06-29 23:08:48 +0530

    Add files via upload

---
+++ assets/js/home.js
@@ -0,0 +1,12 @@
+// main Slider======================================================================================================
+
+$(document).ready(function () {
+	$(".main-slider").slick({
+		infinite: true,
+		slidesToShow: 1,
+		autoplay: false,
+		autoplaySpeed: 2000,
+		arrows: false,
+		dots: true,
+	});
+});
